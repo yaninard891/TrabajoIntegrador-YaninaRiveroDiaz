075754e8fb6108ca9665e1cf226b0b3096799cfd
yanina <yaninard267@gmail.com>
2025-07-10 23:40:22 -0300

    añadi dinamica

---
+++ app.js
@@ -0,0 +1,20 @@
+let item={};
+
+const formulario= document.getElementById("formulario");
+const inputTexto= document.getElementById("input-texto");
+const lista= document.getElementById("lista");
+
+const mostrarlista = () => { 
+    lista.innerHTML = item.map((item) => 
+        '<div class="tarjeta"> ${item} </div>').join("");
+
+    };
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const texto = input.value.trim();
+    if (texto === "") return;
+        item.push(texto);
+        input.value = "";
+        mostrarlista();
+    })
