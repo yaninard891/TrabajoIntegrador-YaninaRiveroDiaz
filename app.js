6e8292958ab665baae9f7cc9217152412c4e48f7
yanina <yaninard267@gmail.com>
2025-07-12 18:51:55 -0300

    modificaciones listas

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ let item= [];
 const formulario= document.getElementById("formulario");
 const inputTexto= document.getElementById("input-texto");
 const lista= document.getElementById("lista");
+const limpiarTodo = document.getElementById("limpiar-todo");
 
 const mostrarlista = () => { 
-    lista.innerHTML = item.map((item) => 
-        `<div class="tarjeta"> ${item} </div>`).join("");
+    lista.innerHTML = item.map((item,index) => 
+    `<div class="tarjeta"> ${item}
+                     </div>`).join("");
 
     };
+
+        
+limpiarTodo.addEventListener("click", () => {
+            item = [];
+            mostrarlista();
+        });
+
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
